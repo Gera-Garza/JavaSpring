fcf9b4cec4bf597f12ab1b3cab22758faa20f321
gera-garza <gerardo.guillermo.gt@gmail.com>
2021-07-12 23:34:19 -0500

    creation of query methods in ProductRepository and ProductoCRUD

--- src/main/java/com/platzi/market/persistance/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistance/ProductoRepository.java
@@ -4,11 +4,18 @@ import com.platzi.market.persistance.crud.ProductoCrudRepository;
 import com.platzi.market.persistance.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
-    public List<Producto> getAll(){
-        return (List<Producto>) productoCrudRepository.findAll();
+    public List<Producto> getAll(){ return (List<Producto>) productoCrudRepository.findAll(); }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad,true);
     }
 }
