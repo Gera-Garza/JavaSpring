d8765a8bf29b77be8b6fd41b2d801b609a02bb99
gera-garza <gerardo.guillermo.gt@gmail.com>
2021-07-13 20:45:06 -0500

    implemented API

--- src/main/java/com/platzi/market/domain/repository/ProductRepository.java
+++ src/main/java/com/platzi/market/domain/repository/ProductRepository.java
@@ -1,4 +1,3 @@
-
 package com.platzi.market.domain.repository;
 
 import com.platzi.market.domain.Product;
@@ -8,14 +7,9 @@ import java.util.Optional;
 
 public interface ProductRepository {
     List<Product> getAll();
-
     Optional<List<Product>> getByCategory(int categoryId);
-
     Optional<List<Product>> getScarseProducts(int quantity);
-
     Optional<Product> getProduct(int productId);
-
     Product save(Product product);
-
     void delete(int productId);
 }
--- src/main/java/com/platzi/market/persistence/entity/Producto.java
+++ src/main/java/com/platzi/market/persistence/entity/Producto.java
@@ -1,7 +1,6 @@
-package com.platzi.market.persistance.entity;
+package com.platzi.market.persistence.entity;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 @Table(name = "productos")
